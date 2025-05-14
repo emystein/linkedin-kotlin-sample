7b0406eb164799fd98a5e08b28c7c0b6ac919bbb
Emiliano Menéndez <emystein@gmail.com>
2025-05-14 10:20:25 -0300

    Replace /me with /userinfo endpoint.

--- server/src/main/java/com/example/api/Constants.java
+++ server/src/main/java/com/example/api/Constants.java
@@ -6,7 +6,7 @@ package com.example.api;
 public class Constants {
     public static final String LI_FIND_AD_ACCOUNTS_FOR_USER_ENDPOINT = "https://api.linkedin.com/v2/adAccountUsersV2?q=authenticatedUser&oauth2_access_token=";
     public static final String LI_FIND_USER_ROLES_ENDPOINT = "https://api.linkedin.com/v2/organizationAcls?q=roleAssignee&oauth2_access_token=";
-    public static final String LI_ME_ENDPOINT = "https://api.linkedin.com/v2/me?oauth2_access_token=";
+    public static final String LI_ME_ENDPOINT = "https://api.linkedin.com/v2/userinfo?oauth2_access_token=";
     public static final String TOKEN_INTROSPECTION_ERROR_MESSAGE = "Error introspecting token, service is not initiated";
     public static final String SAMPLE_APP_BASE = "java-sample-application";
     public static final String SAMPLE_APP_VERSION = "version 1.0";
